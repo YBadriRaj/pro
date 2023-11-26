3fefb1a0f5cfafec469b40357a7b4c5572bcf0d7
YBadriRaj <112421786+YBadriRaj@users.noreply.github.com>
2023-11-26 19:14:12 +0530

    Add files via upload

---
+++ New folder (5)/index.js
@@ -0,0 +1,4 @@
+document.getElementById('addContactBtn').addEventListener('click', function() {
+    // Replace the following code with the logic to add contact to the phone
+    alert('Contact added to phone!');
+});
